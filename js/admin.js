621caf3158181bd5d334a3d4fdb5729dba957415
Tony Sanjaya <sanjayatony@gmail.com>
2018-08-30 16:59:09 +0700

    fix typo

--- js/admin.js
+++ js/admin.js
@@ -8,7 +8,7 @@
   }
 
   $(document).ready(function () {
-    $("select[name*='woocomemrce_finpay_environment']").on('change', function (e, data) {
+    $("select[name*='woocommerce_finpay_environment']").on('change', function (e, data) {
       sensitiveOptions();
     });
     sensitiveOptions();
