75caad7973bf868ed22f1bea663d86acc49558d2
Tony Sanjaya <sanjayatony@gmail.com>
2018-09-18 12:10:34 +0700

    add brivast

--- finpay-gateway.php
+++ finpay-gateway.php
@@ -11,29 +11,25 @@ this plugin written based on https://docs.woocommerce.com/document/payment-gatew
 
 add_action( 'plugins_loaded', 'finpay_gateway_init');
 function finpay_gateway_init() {
+
+
+  // #3 Pending Payment
   include 'class/class.finpay-gateway-briva.php';
+  include 'class/class.finpay-gateway-finpay021.php';
+  include 'class/class.finpay-gateway-finpayst021.php';
 }
 
 /**
  * add finpay to payment gateway
+ * I. cc, permatanet, tcash
+ * II. mandiriclickpay
+ * III. briva, finpay021, finpaysyst021, finpaytsel, vabni, vapermata, vastbni, vastpermata,vamandiri, vastmandiri
  */
 function add_finpay_payment_gateway( $methods ) {
   $methods[] = 'WC_Gateway_Finpay_Briva';
+  $methods[] = 'WC_Gateway_Finpay_Finpay021';
+  $methods[] = 'WC_Gateway_Finpay_Finpayst021';
+
   return $methods;
 }
 add_filter( 'woocommerce_payment_gateways', 'add_finpay_payment_gateway' );
-
-/**
-* Add content to the WC emails.
-*
-* @access public
-* @param WC_Order $order
-* @param bool $sent_to_admin
-* @param bool $plain_text
-*/
-// function email_instructions( $order, $sent_to_admin, $plain_text = false ) {
-//   if ( $this->instructions && ! $sent_to_admin && 'finpay' === $order->payment_method && $order->has_status( 'on-hold' ) ) {
-//       echo wpautop( wptexturize( $this->instructions ) ) . PHP_EOL;
-//   }
-// }
-
